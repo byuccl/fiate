ea0549ac9d0c080ab4c18be618c1c369b2a7d405
Andrew E Wilson <lavatech.co@gmail.com>
2021-04-08 18:01:26 -0600

    Update for MB duts and flash cfgmems

--- step_5_static_par.tcl
+++ step_5_static_par.tcl
@@ -2,7 +2,7 @@ puts "Step 5 Static PAR"
 
 open_checkpoint bld/static/static.dcp
 
-read_checkpoint -cell design_1_i/dut_0/dut_wrapper/U0/mbb_i bld/cpus/vex_tmr.dcp
+read_checkpoint -cell design_1_i/dut_0/dut_wrapper/U0/mbb_i bld/cpus/mb_tmr.dcp
 
 set_property HD.RECONFIGURABLE 1 [get_cells design_1_i/dut_0/dut_wrapper/U0/mbb_i]
 create_pblock pb0
@@ -11,7 +11,7 @@ resize_pblock pb0 -add {SLICE_X0Y0:SLICE_X83Y99 DSP48_X0Y0:DSP48_X4Y39 RAMB18_X0
 set_property RESET_AFTER_RECONFIG true [get_pblocks pb0]
 set_property SNAPPING_MODE ON [get_pblocks pb0]
 
-read_checkpoint -cell design_1_i/dut_1/dut_wrapper/U0/mbb_i bld/cpus/vex_tmr.dcp
+read_checkpoint -cell design_1_i/dut_1/dut_wrapper/U0/mbb_i bld/cpus/mb_tmr.dcp
 
 set_property HD.RECONFIGURABLE 1 [get_cells design_1_i/dut_1/dut_wrapper/U0/mbb_i]
 create_pblock pb1
@@ -20,7 +20,7 @@ resize_pblock pb1 -add {SLICE_X84Y0:SLICE_X163Y99 DSP48_X5Y0:DSP48_X8Y39 RAMB18_
 set_property RESET_AFTER_RECONFIG true [get_pblocks pb1]
 set_property SNAPPING_MODE ON [get_pblocks pb1]
 
-read_checkpoint -cell design_1_i/dut_2/dut_wrapper/U0/mbb_i bld/cpus/vex_tmr.dcp
+read_checkpoint -cell design_1_i/dut_2/dut_wrapper/U0/mbb_i bld/cpus/mb_tmr.dcp
 
 set_property HD.RECONFIGURABLE 1 [get_cells design_1_i/dut_2/dut_wrapper/U0/mbb_i]
 create_pblock pb2
@@ -39,6 +39,8 @@ route_design -directive AggressiveExplore -tns_cleanup
 
 write_checkpoint bld/dcps/static.dcp
 write_bitstream  bld/bits/static.bit
+
+write_cfgmem -format mcs -size 16 -interface SPIx4 -force -loadbit "up 0x00000000 bld/bits/static.bit" -file bld/cfgmem/static.mcs
 
 update_design -cells design_1_i/dut_0/dut_wrapper/U0/mbb_i -black_box
 update_design -cells design_1_i/dut_1/dut_wrapper/U0/mbb_i -black_box
